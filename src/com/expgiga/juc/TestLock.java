5e1903f2dd9fc56ac15c99ac3ce01a15537f8048
yaowang0 <wyaocn@foxmail.com>
2017-08-23 22:54:08 +0800

    Lock

---
+++ src/com/expgiga/juc/TestLock.java
@@ -0,0 +1,52 @@
+package com.expgiga.juc;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 一、解决多线程安全问题：
+ * synchronized：隐式锁
+ * 1.同步代码块
+ * 2.同步方法
+ * jdk1.5以后，
+ * Lock 显式锁
+ * 3.锁
+ * 注意：需要通过lock()方法上锁，通过unlock()方法解锁
+ *
+ * 二、
+ */
+public class TestLock {
+    public static void main(String[] args) {
+        Ticket ticket = new Ticket();
+
+        new Thread(ticket, "1号窗口").start();
+        new Thread(ticket, "2号窗口").start();
+        new Thread(ticket, "3号窗口").start();
+    }
+}
+
+class Ticket implements Runnable {
+
+    private int tick = 100;
+
+    private Lock lock = new ReentrantLock();
+
+    @Override
+    public void run() {
+        while (true) {
+            lock.lock();
+            try {
+                if (tick > 0) {
+                    try {
+                        Thread.sleep(200);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(Thread.currentThread().getName() + " 完成售票，余票为：" + --tick);
+                }
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+}
